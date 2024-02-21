367f2fcd09fdbb0518ea9b59066465513879d528
Luis Arrieta <luisarrieta796@gmail.com>
2024-02-21 10:55:20 -0500

    Install and configure TailwindCSS and Shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function App (): JSX.Element {
   return (
-    <div>Hello world</div>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      Hello world
+      <Button>Shadcn</Button>
+    </div>
   )
 }
