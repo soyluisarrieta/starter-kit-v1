0a9a0418e58174e649c15042ef1225a92f0dcccf
Luis Arrieta <luisarrieta796@gmail.com>
2024-02-21 11:18:06 -0500

    Add Shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button'
+import { Toaster } from '@/components/ui/sonner'
 
 export default function App (): JSX.Element {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       Hello world
       <Button>Shadcn</Button>
+      <Toaster />
     </div>
   )
 }
