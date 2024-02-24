fa544b30b0cce5be3db6e269b6f292ec3f5c73bd
Luis Arrieta <luisarrieta796@gmail.com>
2024-02-23 23:08:30 -0500

    Add constant messages

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const MESSAGE = {
+  WELCOME: '¡Bienvenido a tu panel administrativo!',
+  LOGOUT: 'Sesión finalizada correctamente',
+  ERROR_TRYCATCH: 'Se ha producido un error inesperado. Por favor, inténtelo de nuevo más tarde.'
+}
