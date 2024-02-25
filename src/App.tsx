8f4fcc073bbb13028ac1ca209dc3e5a22f1ec244
Luis Arrieta <luisarrieta796@gmail.com>
2024-02-24 23:58:37 -0500

    Create router usign wouter and protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
-import { Route, Switch } from 'wouter'
+import { Route, Switch, useLocation } from 'wouter'
+import { layoutOrder, layouts, routes } from '@/lib/wouter/router'
+import { useCheckAuth } from '@/hooks/useCheckAuth'
+import { useAuth } from '@/hooks/useAuth'
+import Authenticating from '@/components/pages/Authenticating'
 import MasterLayout from '@/components/layouts/MasterLayout'
-import PublicLayout from '@/components/layouts/PublicLayout'
-import AdminLayout from '@/components/layouts/AdminLayout'
-import LoginPage from '@/components/pages/LoginPage'
-import DashboardPage from '@/components/pages/DashboardPage'
 
 export default function App (): JSX.Element {
+  const isSessionVerified = useCheckAuth()
+  const { isAuth } = useAuth()
+  const [location, setLocation] = useLocation()
+
+  // Checking authentication
+  if (isAuth && !isSessionVerified) {
+    return <Authenticating />
+  }
+
   return (
     <MasterLayout>
       <Switch>
-        <PublicLayout>
-          <Route path="/" component={LoginPage} />
-        </PublicLayout>
-        <AdminLayout>
-          <Route path="/" component={DashboardPage} />
-        </AdminLayout>
+      {routes.map(({ path, component }) => {
+        const numberComponents = Object.keys(component).length
+        const components = layoutOrder.map((layoutPriorized) => {
+          const Layout = layouts[layoutPriorized](isAuth)
+          if (!Layout) {
+            if (numberComponents === 1 && location === path) {
+              setLocation('/ingresar', { state: { from: location }, replace: true })
+            }
+            return null
+          }
+
+          const Component = component[layoutPriorized] || null
+          if (!Component) return null
+
+          return (
+              <Route path={path} key={path}>
+                <Layout>
+                  <Component />
+                </Layout>
+              </Route>
+          )
+        })
+        return components.filter(Boolean)
+      })}
+
+        <Route>404: No such page!</Route>
       </Switch>
     </MasterLayout>
   )
---
+++ src/lib/wouter/router.ts
@@ -0,0 +1,34 @@
+import AdminLayout from '@/components/layouts/AdminLayout'
+import GuestLayout from '@/components/layouts/GuestLayout'
+import PublicLayout from '@/components/layouts/PublicLayout'
+import DashboardPage from '@/components/pages/DashboardPage'
+import HomePage from '@/components/pages/HomePage'
+import LoginPage from '@/components/pages/LoginPage'
+import UsersPage from '@/components/pages/UsersPage'
+
+interface ILayout {
+  children: React.ReactNode
+}
+
+type LayoutComponent = React.FC<ILayout> | null
+export type LayoutMap = Record<string, (isAuth?: boolean) => LayoutComponent | false>
+
+export const layouts: LayoutMap = {
+  private: (isAuth) => isAuth ? AdminLayout : false,
+  guest: () => GuestLayout,
+  public: () => PublicLayout
+}
+
+export const layoutOrder = ['private', 'guest', 'public']
+
+interface Route {
+  path: string
+  component: Record<string, React.FC<any>>
+  redirectTo?: string
+}
+
+export const routes: Route[] = [
+  { path: '/', component: { public: HomePage, private: DashboardPage } },
+  { path: '/ingresar', component: { guest: LoginPage } },
+  { path: '/usuarios', component: { private: UsersPage } }
+]
