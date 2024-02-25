acc3e61d8f85e4edc9f5675e893423b9ca66e8c4
Luis Arrieta <luisarrieta796@gmail.com>
2024-02-24 23:54:03 -0500

    Add test link and clean component pages

--- src/components/pages/DashboardPage.tsx
+++ src/components/pages/DashboardPage.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { Link } from 'wouter'
+
 export default function DashboardPage (): JSX.Element {
   return (
-    <div>DashboardPage</div>
+    <div>
+      DashboardPage
+      <br />
+      <Link to='/usuarios'><Button>Usuarios</Button></Link>
+    </div>
   )
 }
--- src/components/pages/UsersPage.tsx
+++ src/components/pages/UsersPage.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@/components/ui/button'
+import { Link } from 'wouter'
+
 export default function UsersPage (): JSX.Element {
   return (
-    <div>UsersPage</div>
+    <div>UsersPage
+      <br />
+      <Link to='/'><Button>Dashboard</Button></Link>
+    </div>
   )
 }
