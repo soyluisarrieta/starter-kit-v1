918ca0f731ad27ade000ace333d792585875eb66
Luis Arrieta <luisarrieta796@gmail.com>
2024-02-29 14:48:23 -0500

    Refactor LoginForm

---
+++ src/components/modules/auth/LoginForm.tsx
@@ -0,0 +1,42 @@
+import FormField from '@/components/ui/FormField'
+import { Button } from '@/components/ui/button'
+import { useYupValidationResolver } from '@/lib/yup/useYupValidationResolver'
+import { loginSchema } from '@/lib/yup/userSchemas'
+import { useForm } from 'react-hook-form'
+
+interface LoginFormValues {
+  email: string
+  password: string
+}
+
+export default function LoginForm (): JSX.Element {
+  const initialValues: LoginFormValues = { email: '', password: '' }
+  const resolver = useYupValidationResolver(loginSchema)
+
+  const { handleSubmit, register, formState: { errors } } = useForm({
+    resolver,
+    defaultValues: initialValues
+  })
+
+  const onSubmit = (data: LoginFormValues): void => {
+    console.log({ data })
+  }
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormField
+        id='email'
+        label='Correo electrónico'
+        register={register}
+        error={errors.email}
+      />
+      <FormField
+        id='password'
+        label='Contraseña'
+        type='password'
+        register={register}
+        error={errors.password}
+      />
+      <Button type='submit'>Ingresar</Button>
+    </form>
+  )
+}
--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,45 +1,10 @@
-import { loginSchema } from '@/lib/yup/userSchemas'
-import { useForm } from 'react-hook-form'
-import { Button } from '@/components/ui/button'
-import { useYupValidationResolver } from '@/lib/yup/useYupValidationResolver'
-import FormField from '@/components/ui/FormField'
-
-interface LoginFormValues {
-  email: string
-  password: string
-}
+import LoginForm from '@/components/modules/auth/LoginForm'
 
 export default function LoginPage (): JSX.Element {
-  const initialValues: LoginFormValues = { email: '', password: '' }
-  const resolver = useYupValidationResolver(loginSchema)
-  const { handleSubmit, register, formState: { errors } } = useForm({
-    resolver,
-    defaultValues: initialValues
-  })
-
-  const onSubmit = (data: LoginFormValues): void => {
-    console.log({ data })
-  }
-
   return (
     <main>
       <h1>Iniciar sesión</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FormField
-          id='email'
-          label='Correo electrónico'
-          register={register}
-          error={errors.email}
-        />
-        <FormField
-          id='password'
-          label='Contraseña'
-          type='password'
-          register={register}
-          error={errors.password}
-        />
-        <Button type='submit'>Ingresar</Button>
-      </form>
+      <LoginForm />
     </main>
   )
 }
