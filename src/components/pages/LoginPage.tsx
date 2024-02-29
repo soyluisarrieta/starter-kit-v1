cd12cd0286739a30e1fa2799a7ff70ccc709e056
Luis Arrieta <luisarrieta796@gmail.com>
2024-02-29 14:41:23 -0500

    Refactor form field ui

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,9 +1,8 @@
 import { loginSchema } from '@/lib/yup/userSchemas'
 import { useForm } from 'react-hook-form'
 import { Button } from '@/components/ui/button'
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
 import { useYupValidationResolver } from '@/lib/yup/useYupValidationResolver'
+import FormField from '@/components/ui/FormField'
 
 interface LoginFormValues {
   email: string
@@ -26,18 +25,19 @@ export default function LoginPage (): JSX.Element {
     <main>
       <h1>Iniciar sesión</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Label htmlFor='email'>Correo electrónico</Label>
-        <Input id='email' {...register('email')} />
-        {errors.email && (
-          <div role="alert">{errors.email.message?.toString()}</div>
-        )}
-
-        <Label htmlFor='password'>Contraseña</Label>
-        <Input id='password' type='password' {...register('password')} />
-        {errors.password && (
-          <div role="alert">{errors.password.message?.toString()}</div>
-        )}
-
+        <FormField
+          id='email'
+          label='Correo electrónico'
+          register={register}
+          error={errors.email}
+        />
+        <FormField
+          id='password'
+          label='Contraseña'
+          type='password'
+          register={register}
+          error={errors.password}
+        />
         <Button type='submit'>Ingresar</Button>
       </form>
     </main>
