0edfca4fa410d87cbc4c9a0ba45fbba7bf9d9c51
Luis Arrieta <luisarrieta796@gmail.com>
2024-02-29 15:35:41 -0500

    Add headers in axios endpoint

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,5 +2,6 @@ import axios from 'axios'
 
 export default axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL_LOCAL as string,
+  headers: { 'Content-Type': 'application/json' },
   withCredentials: true
 })
