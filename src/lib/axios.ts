5663c179fd736a80007d1d6b060a02e7540d8d58
Luis Arrieta <luisarrieta796@gmail.com>
2024-03-05 15:26:10 -0500

    Add props to axios intance and axios interceptors response for errors and validations

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,38 @@
-import axios from 'axios'
+import Axios, { type AxiosError } from 'axios'
 
-export default axios.create({
+interface ErrorProps {
+  status: number
+  original: AxiosError
+  validation: Record<string, string>
+  message: null | string
+}
+
+const axios = Axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL_LOCAL as string,
-  headers: { 'Content-Type': 'application/json' },
-  withCredentials: true
+  headers: { Accept: 'application/json' },
+  timeout: 60000,
+  withCredentials: true,
+  xsrfCookieName: 'XSRF-TOKEN',
+  xsrfHeaderName: 'X-XSRF-TOKEN'
 })
+
+axios.interceptors.response.use(null, async (err) => {
+  const error: ErrorProps = {
+    status: err.response?.status,
+    original: err,
+    validation: {},
+    message: null
+  }
+
+  if (error.status === 422) {
+    for (const field in err.response.data.errors) {
+      error.validation[field] = err.responsne.data.errors[field][0]
+    }
+  } else {
+    error.message = 'Algo salió mal. Por favor, inténtelo de nuevo más tarde.'
+  }
+
+  return await Promise.reject(error)
+})
+
+export default axios
