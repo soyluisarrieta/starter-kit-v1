4cd6cb1225edafe0d2f18fcb6b3115273ce3b546
Luis Arrieta <luisarrieta796@gmail.com>
2024-03-06 11:53:33 -0500

    Change axios instance on check auth custom hook

--- src/hooks/useCheckAuth.ts
+++ src/hooks/useCheckAuth.ts
@@ -1,18 +1,20 @@
-import { useAuth } from '@/hooks/useAuth'
-import { useSessionVerified } from '@/store/AuthStore'
+import { axiosPublic } from '@/lib/axios'
+import { useAuthStore, useSessionVerified } from '@/store/AuthStore'
 import nProgress from 'nprogress'
 import { useEffect } from 'react'
 
 export function useCheckAuth (): boolean {
-  const { getProfile } = useAuth()
+  const { setProfile } = useAuthStore()
   const { sessionVerified, setSessionVerified } = useSessionVerified()
 
   const checkAuthentication = async (): Promise<void> => {
     nProgress.start()
     try {
-      getProfile()
-    } catch (e) {
-      console.warn(e)
+      const userData: ProfileAuth = await axiosPublic('/api/user')
+      setProfile(userData)
+    } catch (err) {
+      console.warn(err)
+      setProfile(null)
     } finally {
       setSessionVerified(true)
       nProgress.done()
