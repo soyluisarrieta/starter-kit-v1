77b1a65007e5b9341fbbc15f4eb50f0221582026
Luis Arrieta <luisarrieta796@gmail.com>
2024-03-06 18:03:47 -0500

    Add logout button in dashboard page

--- src/components/pages/DashboardPage.tsx
+++ src/components/pages/DashboardPage.tsx
@@ -1,12 +1,16 @@
 import { Button } from '@/components/ui/button'
+import { useAuth } from '@/hooks/useAuth'
 import { Link } from 'wouter'
 
 export default function DashboardPage (): JSX.Element {
+  const { logout } = useAuth()
+
   return (
     <div>
       DashboardPage
       <br />
       <Link to='/usuarios'><Button>Usuarios</Button></Link>
+      <Button variant='destructive' onClick={logout}>Cerrar sesión</Button>
     </div>
   )
 }
