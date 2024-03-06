4504f1636decc1d13aadc1962c79311517db7cc1
Luis Arrieta <luisarrieta796@gmail.com>
2024-03-06 16:13:54 -0500

    Add attributes in registered user controller and validations

--- API-cv-editor/app/Http/Controllers/Auth/RegisteredUserController.php
+++ API-cv-editor/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
@@ -21,15 +21,28 @@ class RegisteredUserController extends Controller
     public function store(Request $request): Response
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'alpha', 'max:25'],
+            'last_name' => ['required', 'string', 'alpha', 'max:25'],
+            'birthdate' => ['nullable', 'date'],
+            'gender' => ['nullable', 'in:male,female,other'],
+            'phone' => ['nullable', 'numeric', 'max:10'],
+            'address' => ['nullable', 'string', 'max:255'],
+            'avatar' => ['nullable', 'string', 'max:50'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', Password::min(8)->letters()->numbers()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'last_name' => $request->last_name,
+            'birthdate' => $request->birthdate,
+            'gender' => $request->gender,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'avatar' => $request->avatar,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'active' => true,
         ]);
 
         event(new Registered($user));
