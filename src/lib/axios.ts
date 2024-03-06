72cb232d362649a8735cf2fd60c6bb586466caeb
Luis Arrieta <luisarrieta796@gmail.com>
2024-03-05 22:45:55 -0500

    Add comments and show toast sonner with error message in axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,14 @@
 import Axios, { type AxiosError } from 'axios'
+import { toast } from 'sonner'
 
-interface ErrorProps {
+export interface ErrorProps {
   status: undefined | number
   original: AxiosError
   validation: Record<string, string>
   message: null | string
 }
 
+// Axios instance
 const axios = Axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL_LOCAL as string,
   headers: { Accept: 'application/json' },
@@ -15,6 +17,7 @@ const axios = Axios.create({
   withXSRFToken: true
 })
 
+// Error handler usign axios interceptors
 axios.interceptors.response.use(null, async (err) => {
   const error: ErrorProps = {
     status: err.response?.status,
@@ -23,18 +26,10 @@ axios.interceptors.response.use(null, async (err) => {
     message: null
   }
 
-  if (error.status === 422) {
-    for (const field in err.response.data.errors) {
-      error.validation[field] = err.responsne.data.errors[field][0]
-    }
-  } else {
-    error.message = 'Algo salió mal. Por favor, inténtelo de nuevo más tarde.'
-  }
-
   switch (error.status) {
     case 422:
       for (const field in err.response.data.errors) {
-        error.validation[field] = err.responsne.data.errors[field][0]
+        error.validation[field] = err.response.data.errors[field][0]
       }
       break
     case 403:
@@ -50,6 +45,7 @@ axios.interceptors.response.use(null, async (err) => {
       error.message = 'Algo salió mal. Por favor, inténtelo de nuevo más tarde.'
   }
 
+  toast(error.message)
   return await Promise.reject(error)
 })
 
