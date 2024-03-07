80c4b362220f0000c4d79a4acaff661cd80c7f5b
Luis Arrieta <luisarrieta796@gmail.com>
2024-03-07 11:56:39 -0500

    Create uses uuid trait and add trait in user model

--- API-cv-editor/app/Models/User.php
+++ API-cv-editor/app/Models/User.php
@@ -6,37 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Support\Str;
+use App\Traits\UsesUuid;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The "booted" method of the model.
-     *
-     * @return void
-     */
-    protected static function booted()
-    {
-        static::creating(function ($model) {
-            $model->{$model->getKeyName()} = (string) Str::uuid();
-        });
-    }
-
-    /**
-     * Indicates if the IDs are auto-incrementing.
-     *
-     * @var bool
-     */
-    public $incrementing = false;
-
-    /**
-     * The data type of the auto-incrementing ID.
-     *
-     * @var string
-     */
-    protected $keyType = 'string';
+    use UsesUuid, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -44,7 +18,6 @@ class User extends Authenticatable
      * @var array<string>
      */
     protected $fillable = [
-        'id',
         'name',
         'last_name',
         'birthdate',
