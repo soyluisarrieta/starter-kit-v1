252fad40581f9c3b0f4cf6f19292a7ca247d5d49
Luis Arrieta <luisarrieta796@gmail.com>
2024-03-12 22:20:46 -0500

    Add user columns migration

--- API_LaravelBreeze/database/migrations/0001_01_01_000000_create_users_table.php
+++ API_LaravelBreeze/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->uuid('id')->primary();
+            $table->string('name', 25);
+            $table->string('last_name', 25);
+            $table->date('birthdate')->nullable();
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->string('phone', 10)->nullable();
+            $table->string('address')->nullable();
+            $table->string('avatar', 50)->nullable();
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password', 100);
+            $table->timestamp('last_activity')->nullable();
             $table->timestamps();
+            $table->rememberToken();
+            $table->boolean('active')->default(true);
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
