5e8848d40ae3a7a7a7ec11ed0840bc4c50deb5a2
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-13 18:00:01 -0500

    Add shadcn form component in RegisterForm

--- src/components/modules/auth/RegisterForm.tsx
+++ src/components/modules/auth/RegisterForm.tsx
@@ -1,10 +1,12 @@
 import nProgress from 'nprogress'
-import FormField from '@/components/ui/FormField'
 import { Button } from '@/components/ui/button'
 import { useAuth } from '@/hooks/useAuth'
 import { useYupValidationResolver } from '@/lib/yup/useYupValidationResolver'
 import { registerSchema } from '@/lib/yup/userSchemas'
 import { useForm } from 'react-hook-form'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 import { csrfService, registerService } from '@/services/authService'
 import { toast } from 'sonner'
 import { MESSAGE } from '@/constants'
@@ -12,24 +14,24 @@ import { handleValidationErrors } from '@/lib/utils/handleValidationErrors'
 
 export default function RegisterForm (): JSX.Element {
   const { getProfile } = useAuth()
-  const initialValues: RegisterForm = {
-    name: '',
-    last_name: '',
-    email: '',
-    password: '',
-    password_confirmation: ''
-  }
 
-  const resolver = useYupValidationResolver(registerSchema)
-
-  const { handleSubmit, register, formState: { errors }, setError } = useForm({
-    resolver,
-    defaultValues: initialValues
+  // Form config
+  const form = useForm({
+    resolver: useYupValidationResolver(registerSchema),
+    defaultValues: {
+      name: '',
+      last_name: '',
+      gender: undefined,
+      email: '',
+      password: '',
+      password_confirmation: ''
+    }
   })
 
   // Fn: Send register user data to API
   const onRegister = async (userData: RegisterForm): Promise<void> => {
     nProgress.start()
+
     try {
       await csrfService()
       await registerService(userData)
@@ -38,47 +40,128 @@ export default function RegisterForm (): JSX.Element {
       toast(MESSAGE.WELCOME, { position: 'top-right', duration: 5000 })
     } catch (err) {
       console.warn(err)
-      handleValidationErrors(err, setError)
+      handleValidationErrors(err, form.setError)
     } finally {
       nProgress.done()
     }
   }
 
   return (
-    <form onSubmit={handleSubmit(onRegister)}>
-      <FormField
-        id='name'
-        label='Nombre'
-        register={register}
-        error={errors.name}
-      />
-      <FormField
-        id='last_name'
-        label='Apellido'
-        register={register}
-        error={errors.last_name}
-      />
-      <FormField
-        id='email'
-        label='Correo electrónico'
-        register={register}
-        error={errors.email}
-      />
-      <FormField
-        id='password'
-        label='Contraseña'
-        type='password'
-        register={register}
-        error={errors.password}
-      />
-      <FormField
-        id='password_confirmation'
-        label='Confirmar contraseña'
-        type='password'
-        register={register}
-        error={errors.password_confirmation}
-      />
-      <Button type='submit'>Registrarse</Button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onRegister)}>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nombre</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="last_name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Apellido</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+         <FormField
+          control={form.control}
+          name="gender"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Género</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col space-y-1"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="male" />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Masculino
+                    </FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="female" />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Femenino
+                    </FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="other2" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Otro</FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Correo electrónico</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Contraseña</FormLabel>
+              <FormControl>
+                <Input type='password' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password_confirmation"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirmar contraseña</FormLabel>
+              <FormControl>
+                <Input type='password' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type='submit'>Registrarse</Button>
+      </form>
+    </Form>
   )
 }
