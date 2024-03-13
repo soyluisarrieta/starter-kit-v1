d7f01f347d4da459277e5f52d213cd8a31893289
Luis Arrieta <luisarrieta796@gmail.com>
2024-03-12 22:26:15 -0500

    Add uses uuid trait and use in user model

--- API_LaravelBreeze/app/Models/User.php
+++ API_LaravelBreeze/app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Traits\UsesUuid;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use UsesUuid, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
