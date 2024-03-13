af0f20f428aac3c3107297da3a2481703842c02f
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-13 18:07:07 -0500

    Add shadcn form components in LoginForm

--- src/components/modules/auth/LoginForm.tsx
+++ src/components/modules/auth/LoginForm.tsx
@@ -1,5 +1,4 @@
 import nProgress from 'nprogress'
-import FormField from '@/components/ui/FormField'
 import { Button } from '@/components/ui/button'
 import { useAuth } from '@/hooks/useAuth'
 import { useYupValidationResolver } from '@/lib/yup/useYupValidationResolver'
@@ -10,17 +9,22 @@ import { navigate } from 'wouter/use-browser-location'
 import { toast } from 'sonner'
 import { MESSAGE } from '@/constants'
 import { handleValidationErrors } from '@/lib/utils/handleValidationErrors'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 
 export default function LoginForm (): JSX.Element {
   const { getProfile } = useAuth()
-  const initialValues: Credentials = { email: '', password: '' }
-  const resolver = useYupValidationResolver(loginSchema)
 
-  const { handleSubmit, register, formState: { errors }, setError } = useForm({
-    resolver,
-    defaultValues: initialValues
+  // Form config
+  const form = useForm({
+    resolver: useYupValidationResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
   })
 
+  // Get before route
   const from: string = (history.state?.from) !== undefined
     ? history.state.from
     : '/'
@@ -38,28 +42,45 @@ export default function LoginForm (): JSX.Element {
       toast(MESSAGE.WELCOME, { position: 'top-right', duration: 5000 })
     } catch (err: any) {
       console.warn(err)
-      handleValidationErrors(err, setError)
+      handleValidationErrors(err, form.setError)
     } finally {
       nProgress.done()
     }
   }
 
   return (
-    <form onSubmit={handleSubmit(onLogin)}>
-      <FormField
-        id='email'
-        label='Correo electrónico'
-        register={register}
-        error={errors.email}
-      />
-      <FormField
-        id='password'
-        label='Contraseña'
-        type='password'
-        register={register}
-        error={errors.password}
-      />
-      <Button type='submit'>Ingresar</Button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onLogin)}>
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Correo electrónico</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Contraseña</FormLabel>
+              <FormControl>
+                <Input type='password' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type='submit'>Ingresar</Button>
+      </form>
+    </Form>
   )
 }
