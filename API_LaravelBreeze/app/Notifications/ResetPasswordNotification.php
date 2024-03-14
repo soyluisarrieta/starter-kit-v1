592086c57cc22dcbf7d98746e44ed4a26d6c0cb2
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-14 11:29:22 -0500

    Change url to spanish in ResetPasswordNotification and configure in user model

---
+++ API_LaravelBreeze/app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * The password reset token.
+     *
+     * @var string
+     */
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = env('FRONTEND_URL').'/restablecer-contrasena/'.$this->token.'?email='.urlencode($notifiable->getEmailForPasswordReset());
+
+        return (new MailMessage)
+            ->line('Recibes este correo electrónico porque recibimos una solicitud de restablecimiento de contraseña para tu cuenta XDD.')
+            ->action('Reestablecer Contraseña', $url)
+            ->line('Si no solicitaste un restablecimiento de contraseña, no se requiere ninguna otra acción.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
