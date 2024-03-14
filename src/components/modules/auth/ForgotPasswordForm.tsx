04b910fd339e111fde65be2629bec7c61b3d962f
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-13 21:37:39 -0500

    Add try again in ForgotPasswordForm

--- src/components/modules/auth/ForgotPasswordForm.tsx
+++ src/components/modules/auth/ForgotPasswordForm.tsx
@@ -1,15 +1,31 @@
+import nProgress from 'nprogress'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { handleValidationErrors } from '@/lib/utils/handleValidationErrors'
 import { useYupValidationResolver } from '@/lib/yup/useYupValidationResolver'
 import { forgotPwSchema } from '@/lib/yup/userSchemas'
 import { csrfService, forgotPwService } from '@/services/authService'
-import nProgress from 'nprogress'
-import { Button } from 'react-day-picker'
 import { useForm } from 'react-hook-form'
-import { toast } from 'sonner'
+import { Button } from '@/components/ui/button'
+import { useState, useEffect } from 'react'
+import { TimerIcon } from 'lucide-react'
 
 export default function ForgotPasswordForm (): JSX.Element {
+  const [isSent, setIsSent] = useState(false)
+  const [counter, setCounter] = useState(60) // Nuevo estado para el contador
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout
+    if (isSent && counter > 0) {
+      timer = setInterval(() => { setCounter(counter - 1) }, 1000)
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer)
+      }
+    }
+  }, [isSent, counter])
+
   // Form config
   const form = useForm({
     resolver: useYupValidationResolver(forgotPwSchema),
@@ -22,8 +38,8 @@ export default function ForgotPasswordForm (): JSX.Element {
     try {
       await csrfService()
       await forgotPwService(email)
-      toast('¡Enlace de recuperación enviado!', { position: 'top-right', duration: 5000 })
       form.reset()
+      setIsSent(true)
     } catch (err: any) {
       console.warn(err)
       handleValidationErrors(err, form.setError)
@@ -31,25 +47,46 @@ export default function ForgotPasswordForm (): JSX.Element {
       nProgress.done()
     }
   }
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onForgotPassword)}>
-        <FormField
-          control={form.control}
-          name='email'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Correo electrónico</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
 
-        <Button type='submit'>Enviar link</Button>
-      </form>
-    </Form>
-  )
+  return !isSent
+    ? (
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onForgotPassword)}>
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Correo electrónico</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type='submit'>Reestablecer contraseña</Button>
+        </form>
+      </Form>
+      )
+    : (
+      <div>
+        <h3>¡Listo!</h3>
+        <p>Le hemos enviado por correo electrónico el enlace para restablecer su contraseña.</p>
+        <p>¿Aún no has recibido el enlace?</p>
+        <div className='flex items-center'>
+          <Button
+            disabled={counter !== 0}
+            onClick={() => {
+              setIsSent(false)
+              setCounter(60)
+            }}
+          >
+            Volver a intentar
+          </Button>
+          {counter !== 0 && (<><TimerIcon className='ml-3 mr-1' size={16} /> {counter} </>)}
+        </div>
+      </div>
+      )
 }
--- src/components/pages/Auth/ForgotPasswordPage.tsx
+++ src/components/pages/Auth/ForgotPasswordPage.tsx
@@ -2,19 +2,12 @@ import { Button } from '@/components/ui/button'
 import { Link } from 'wouter'
 import ForgotPasswordForm from '@/components/modules/auth/ForgotPasswordForm'
 
-export default function ForgotPassword (): JSX.Element {
+export default function ForgotPasswordPage (): JSX.Element {
   return (
     <main>
       <h1>Recuperar contraseña</h1>
       <ForgotPasswordForm />
 
-      <div>
-        <Link to='/registrarse'>
-          <Button variant='link'>
-            ¿Deseas volver a intentar?
-          </Button>
-        </Link>
-      </div>
       <div>
         ¿No tienes una cuenta?
         <Link to='/registrarse'>
