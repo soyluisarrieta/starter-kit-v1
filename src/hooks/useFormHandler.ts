4a6eadb618914e07683f4d8b9bc889656cb17c34
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-15 16:29:41 -0500

    Add form handler custom hook

---
+++ src/hooks/useFormHandler.ts
@@ -0,0 +1,82 @@
+import { handleValidationErrors } from '@/lib/utils/handleValidationErrors'
+import { useYupValidationResolver } from '@/lib/yup/useYupValidationResolver'
+import { csrfService } from '@/services/authService'
+import nProgress from 'nprogress'
+import { useState } from 'react'
+import { type UseFormReturn, type UseFormProps, useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { navigate } from 'wouter/use-browser-location'
+import { type AnyObjectSchema } from 'yup'
+
+interface FormHandlerProps {
+  withCsrf?: boolean
+  schema: AnyObjectSchema
+  request: (data: any) => Promise<any>
+  onError?: () => Promise<void>
+  onFinally?: () => Promise<void>
+  successMessage?: string
+  redirectTo?: string
+  defaultValues?: Record<string, any>
+  formConfig?: UseFormProps
+}
+
+interface FormHandler {
+  form: UseFormReturn
+  onSubmit: (data: any) => Promise<any>
+  response: Record<string, any>
+  isLoading: boolean
+}
+
+export function useFormHandler ({
+  withCsrf = true,
+  schema,
+  request,
+  onError,
+  onFinally,
+  successMessage = '',
+  redirectTo,
+  defaultValues,
+  formConfig
+}: FormHandlerProps): FormHandler {
+  const [isLoading, setIsLoading] = useState(false)
+  const [response, setResponse] = useState<any>(null)
+
+  // Form config
+  const form = useForm({
+    resolver: useYupValidationResolver(schema),
+    defaultValues,
+    ...formConfig
+  })
+
+  // fn: Handle submit
+  const onSubmit = form.handleSubmit(async (data: Record<string, any>): Promise<void> => {
+    nProgress.start()
+    setIsLoading(true)
+    try {
+      if (withCsrf) { await csrfService() }
+      const res = await request(data)
+      setResponse(res)
+      if (successMessage) {
+        toast.success(successMessage, { duration: 5000 })
+      }
+      if (redirectTo) {
+        navigate(redirectTo, { replace: true })
+      }
+    } catch (err) {
+      console.warn(err)
+      handleValidationErrors(err, form.setError)
+      onError && await onError()
+    } finally {
+      nProgress.done()
+      setIsLoading(false)
+      onFinally && await onFinally()
+    }
+  })
+
+  return {
+    form,
+    onSubmit,
+    response,
+    isLoading
+  }
+}
