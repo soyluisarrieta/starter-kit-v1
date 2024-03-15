b44add7397d452765110c4b59f95321d46dc55fd
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-15 16:55:13 -0500

    Use FormHandler in LoginForm

--- src/components/modules/auth/LoginForm.tsx
+++ src/components/modules/auth/LoginForm.tsx
@@ -1,57 +1,45 @@
 import nProgress from 'nprogress'
 import { Button } from '@/components/ui/button'
 import { useAuth } from '@/hooks/useAuth'
-import { useYupValidationResolver } from '@/lib/yup/useYupValidationResolver'
 import { loginSchema } from '@/lib/yup/userSchemas'
-import { useForm } from 'react-hook-form'
-import { csrfService, loginService } from '@/services/authService'
-import { navigate } from 'wouter/use-browser-location'
-import { toast } from 'sonner'
+import { loginService } from '@/services/authService'
 import { MESSAGE } from '@/constants'
-import { handleValidationErrors } from '@/lib/utils/handleValidationErrors'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { useFormHandler } from '@/hooks/useFormHandler'
 
 export default function LoginForm (): JSX.Element {
   const { getProfile } = useAuth()
-
-  // Form config
-  const form = useForm({
-    resolver: useYupValidationResolver(loginSchema),
-    defaultValues: {
-      email: '',
-      password: ''
-    }
-  })
 
   // Get before route
   const from: string = (history.state?.from) !== undefined
     ? history.state.from
     : '/'
 
-  // Fn: Send login credentials to API
-  const onLogin = async (credentials: Credentials): Promise<void> => {
-    nProgress.start()
-    try {
-      await csrfService()
+  const defaultValues = {
+    email: '',
+    password: ''
+  }
+
+  // Form config
+  const { form, onSubmit } = useFormHandler({
+    schema: loginSchema,
+    successMessage: MESSAGE.WELCOME,
+    defaultValues,
+    redirectTo: from,
+    request: async (credentials: Credentials): Promise<void> => {
       await loginService(credentials)
       nProgress.inc(0.4)
       await getProfile()
-
-      navigate(from, { replace: true })
-      toast(MESSAGE.WELCOME, { position: 'top-right', duration: 5000 })
-    } catch (err: any) {
-      console.warn(err)
-      handleValidationErrors(err, form.setError)
-      form.setValue('password', '')
-    } finally {
-      nProgress.done()
+    },
+    onError: ({ form }) => {
+      form?.setValue('password', '')
     }
-  }
+  })
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onLogin)}>
+      <form onSubmit={onSubmit}>
         <FormField
           control={form.control}
           name='email'
