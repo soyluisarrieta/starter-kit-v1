9de5ce969860691f75dff77e06c445693882e498
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-24 15:35:06 -0500

    Refactor link sent notification in ForgotPassoword

--- src/components/modules/auth/ForgotPasswordForm.tsx
+++ src/components/modules/auth/ForgotPasswordForm.tsx
@@ -3,87 +3,40 @@ import { Input } from '@/components/ui/input'
 import { forgotPwSchema } from '@/lib/yup/userSchemas'
 import { forgotPwService } from '@/services/authService'
 import { Button } from '@/components/ui/button'
-import { useState, useEffect } from 'react'
-import { TimerIcon } from 'lucide-react'
 import { useFormHandler } from '@/hooks/useFormHandler'
-import { navigate } from 'wouter/use-browser-location'
 
-export default function ForgotPasswordForm (): JSX.Element {
-  const [isSent, setIsSent] = useState(false)
-  const [counter, setCounter] = useState(60)
+interface Props { setIsLinkSent: (isSent: boolean) => void }
 
-  // Counter
-  useEffect(() => {
-    if (!counter) return
-    const intervalId = setInterval(() => {
-      setCounter(counter - 1)
-    }, 1000)
-    return () => { clearInterval(intervalId) }
-  }, [counter])
-
-  // Redirect if password changed
-  useEffect(() => {
-    const handleStorageChange = (e: StorageEvent): void => {
-      if (e.key === 'passwordChanged' && e.newValue === 'true') {
-        window.localStorage.removeItem('passwordChanged')
-        navigate('/ingresar')
-      }
-    }
-    window.addEventListener('storage', handleStorageChange)
-    return () => {
-      window.removeEventListener('storage', handleStorageChange)
-    }
-  }, [])
-
+export default function ForgotPasswordForm ({ setIsLinkSent }: Props): JSX.Element {
   // Form config
   const { form, onSubmit } = useFormHandler({
     schema: forgotPwSchema,
     defaultValues: { email: '' },
     request: async (email: { email: string }) => {
       await forgotPwService(email)
-      setIsSent(true)
+      setIsLinkSent(true)
     }
   })
 
-  return !isSent
-    ? (
-      <Form {...form}>
-        <form onSubmit={onSubmit}>
-          <FormField
-            control={form.control}
-            name='email'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Correo electrónico</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+  return (
+    <Form {...form}>
+      <form onSubmit={onSubmit}>
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Correo electrónico</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-          <Button type='submit'>Reestablecer contraseña</Button>
-        </form>
-      </Form>
-      )
-    : (
-      <div>
-        <h3>¡Listo!</h3>
-        <p>Le hemos enviado por correo electrónico el enlace para restablecer su contraseña.</p>
-        <p>¿Aún no has recibido el enlace?</p>
-        <div className='flex items-center'>
-          <Button
-            disabled={counter !== 0}
-            onClick={() => {
-              setIsSent(false)
-              setCounter(60)
-            }}
-          >
-            Volver a intentar
-          </Button>
-          {counter !== 0 && (<><TimerIcon className='ml-3 mr-1' size={16} /> {counter} </>)}
-        </div>
-      </div>
-      )
+        <Button type='submit'>Reestablecer contraseña</Button>
+      </form>
+    </Form>
+  )
 }
--- src/components/pages/Auth/ForgotPasswordPage.tsx
+++ src/components/pages/Auth/ForgotPasswordPage.tsx
@@ -1,20 +1,76 @@
+import { useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
-import { Link } from 'wouter'
 import ForgotPasswordForm from '@/components/modules/auth/ForgotPasswordForm'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { navigate } from 'wouter/use-browser-location'
+import { CheckCircleIcon, TimerIcon } from 'lucide-react'
 
 export default function ForgotPasswordPage (): JSX.Element {
-  return (
-    <main>
-      <h1>Recuperar contraseña</h1>
-      <ForgotPasswordForm />
+  const [isLinkSent, setIsLinkSent] = useState(false)
+  const [counter, setCounter] = useState(60)
 
-      <div>
+  // Counter
+  useEffect(() => {
+    if (!counter) return
+    const intervalId = setInterval(() => {
+      setCounter(counter - 1)
+    }, 1000)
+    return () => { clearInterval(intervalId) }
+  }, [counter])
+
+  // Redirect if password changed
+  useEffect(() => {
+    const handleStorageChange = (e: StorageEvent): void => {
+      if (e.key === 'passwordChanged' && e.newValue === 'true') {
+        window.localStorage.removeItem('passwordChanged')
+        navigate('/ingresar')
+      }
+    }
+    window.addEventListener('storage', handleStorageChange)
+    return () => {
+      window.removeEventListener('storage', handleStorageChange)
+    }
+  })
+
+  return (
+    <main className='min-h-screen flex flex-col justify-center items-center p-5'>
+      <Card className='max-w-md w-full'>
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl">
+            {isLinkSent
+              ? <><CheckCircleIcon className='inline-block' size={24} /> ¡Enviado!</>
+              : 'Recuperar contraseña'}
+          </CardTitle>
+          <CardDescription>
+            {isLinkSent
+              ? ('Le hemos enviado por correo electrónico el enlace para restablecer su contraseña.')
+              : ('Ingresa tu correo electrónico para recibir enlace para recuperar tu contraseña.')}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          {isLinkSent
+            ? (
+              <div className='flex items-center'>
+                <Button
+                  disabled={counter !== 0}
+                  onClick={() => { setCounter(60) }}
+                >
+                  Volver a intentar
+                </Button>
+                {counter !== 0 && (<><TimerIcon className='ml-3 mr-1' size={16} /> {counter} </>)}
+              </div>
+              )
+            : (
+              <ForgotPasswordForm setIsLinkSent={setIsLinkSent} />
+              )}
+        </CardContent>
+      </Card>
+
+      <div className='pt-4'>
         ¿No tienes una cuenta?
-        <Link to='/registrarse'>
-          <Button variant='link'>
-            Regístrate
-          </Button>
-        </Link>
+        <Button to='/registrarse' variant='link' className='ml-1.5'>
+          Regístrate
+        </Button>
       </div>
     </main>
   )
