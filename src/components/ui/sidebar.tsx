e126094289b488e118df68c0f13625f0be0ff144
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-29 20:18:17 -0500

    Add improvements to animations in sidebar components

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -8,7 +8,7 @@ import { useScreenSize } from '@/hooks/useScreenSize'
 import { cn } from '@/lib/utils'
 import { useSidebarStore } from '@/store/SidebarStore'
 import { getYear } from 'date-fns'
-import { BellIcon, HelpCircleIcon, type LucideIcon, HistoryIcon, SettingsIcon, LogOutIcon } from 'lucide-react'
+import { BellIcon, HelpCircleIcon, type LucideIcon, HistoryIcon, SettingsIcon, LogOutIcon, MenuIcon } from 'lucide-react'
 import { Link } from 'wouter'
 
 interface MenuItem {
@@ -34,53 +34,57 @@ export default function Sidebar ({ menuItems, user, onLogout }: SidebarProps): J
 
   return (
     <>
-      {lgScreen && <BackdropBlur className={cn('fixed inset-0 z-10 transition-all duration-300', !isSidebarOpen && 'pointer-events-none')} onClick={() => { isSidebarOpen && toggleSidebar() }} style={{ opacity: isSidebarOpen ? 1 : 0 }} />}
+      {!lgScreen && <BackdropBlur className={cn('fixed inset-0 z-10 transition-all duration-200', !isSidebarOpen && 'pointer-events-none')} onClick={() => { isSidebarOpen && toggleSidebar() }} style={{ opacity: isSidebarOpen ? 1 : 0 }} />}
 
-      <aside className={cn('w-10/12 lg:w-72 max-w-72 fixed lg:relative z-10 transition-[width] duration-300 overflow-hidden', !isSidebarOpen && 'w-0 lg:w-[76px]')}>
-          <ScrollArea className='w-full h-dvh bg-background overflow-y-auto border-r'>
-            <div className='h-dvh min-h-fit flex flex-col'>
-              <div className='flex-grow-0 py-4 px-6'>
-                <div>LOGO</div>
-                <div></div>
-              </div>
+      <aside className={cn('w-10/12 lg:w-72 max-w-72 fixed lg:relative z-10 transition-[width] duration-200 overflow-hidden', !isSidebarOpen && 'w-0 lg:w-[76px]')}>
+        <ScrollArea className='w-full h-dvh bg-background overflow-y-auto border-r'>
+          <div className='h-dvh min-h-fit flex flex-col'>
+            <div className='flex-grow-0 py-4 px-2.5 flex items-center'>
+              <div className={cn('w-full transition-[width] duration-200', isSidebarOpen ? 'flex-1' : 'w-0 overflow-hidden')}>LOGO</div>
+              <Button variant='outline' onClick={toggleSidebar}>
+                <MenuIcon />
+              </Button>
+            </div>
 
-              <div className={cn('w-full transition-[width] duration-300 space-y-5 py-4 flex-1', !isSidebarOpen && 'w-[76px]')}>
-                {menuItems.map((section, index) => (
-                  <div key={index} className="px-2.5">
-                    {section.title && (<h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">{section.title}</h2>)}
-                    <ul className="space-y-1">
-                      {section.items.map((item, idx) => (
-                        <li key={idx}>
+            <div className={cn('w-full transition-[width] duration-200 space-y-5 py-4 px-2.5 flex-1', !isSidebarOpen && 'w-[76px]')}>
+              {menuItems.map((section, index) => (
+                <div key={index}>
+                  {section.title && (<h2 className={cn('mb-2 px-2 sm:text-lg font-semibold tracking-tight transition-opacity duration-200 relative', lgScreen && !isSidebarOpen && 'left-0 opacity-0')}>{section.title}</h2>)}
+                  <ul className="space-y-1">
+                    {section.items.map((item, idx) => (
+                      <li key={idx}>
                         <Button
                           variant={index === 0 && idx === 0 ? 'default' : 'ghost'}
-                          className={'w-full'}
+                          className='w-full'
                           to={item.link}
                         >
-                          {item.Icon && <item.Icon size={22} strokeWidth={1.5} style={{ minWidth: 'fit-content' }} />}
-                          <span className={cn('w-full transition-all duration-300 overflow-hidden flex-1', isSidebarOpen ? 'ml-1' : 'flex-1 opacity-0')}>
+                          {item.Icon && <item.Icon className='min-w-fit' size={22} strokeWidth={1.5} />}
+                          <span className={cn('w-full transition-all duration-200 overflow-hidden flex-1', isSidebarOpen ? 'ml-1' : 'flex-1 opacity-0')}>
                             {item.title}
                           </span>
                         </Button>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ))}
-              </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
 
-              <div className="flex-grow-0">
-                <Separator orientation='horizontal' />
+            <div className="flex-grow-0">
+              <Separator orientation='horizontal' />
 
-                <div className="w-full flex items-center gap-1 p-4">
-                  <Link to='/ajustes/perfil'>
-                    <Avatar>
-                      <AvatarImage src={user?.avatar && IMAGES.AVATARS + user.avatar} />
-                      <AvatarFallback className='bg-primary text-primary-foreground'>
-                        {user?.name[0]}{user?.last_name[0]}
-                      </AvatarFallback>
-                    </Avatar>
-                  </Link>
-                  <div className='flex-1 text-left pl-1 whitespace-nowrap overflow-hidden'>
+              <div className="w-full flex items-center gap-1 p-4">
+                <Link to='/ajustes/perfil'>
+                  <Avatar className='outline outline-2 outline-primary outline-offset-2 ml-0.5'>
+                    <AvatarImage src={user?.avatar && IMAGES.AVATARS + user.avatar} />
+                    <AvatarFallback className='bg-primary text-primary-foreground'>
+                      {user?.name[0]}{user?.last_name[0]}
+                    </AvatarFallback>
+                  </Avatar>
+                </Link>
+
+                <div className={cn('w-full transition-[width_opacity] duration-200 flex', !isSidebarOpen && 'w-0 opacity-0')}>
+                  <div className='flex-1 text-left pl-2 whitespace-nowrap overflow-hidden'>
                     <h2 className="text-base font-semibold -mb-1 text-ellipsis overflow-hidden" title={`${user?.name} ${user?.last_name}`}>
                       {user?.name} {user?.last_name}
                     </h2>
@@ -92,45 +96,45 @@ export default function Sidebar ({ menuItems, user, onLogout }: SidebarProps): J
                     <SettingsIcon size={20} strokeWidth={1.5} />
                   </Button>
                 </div>
+              </div>
 
-                <Separator orientation='horizontal' />
+              <Separator orientation='horizontal' />
 
-                <div className="flex-grow-0 px-4">
-                  <ul className="w-full flex items-center justify-between py-3 opacity-85 hover:opacity-100">
-                    <li>
-                      <Button variant='outline' size='icon' className="focus:outline-none focus:ring-2 focus:ring-gray-300">
-                        <BellIcon size={20} strokeWidth={1.75} />
+              <div className={cn('w-full transition-[width] duration-200 overflow-hidden px-4 flex items-center', !isSidebarOpen && 'w-[76px]', (!lgScreen || isSidebarOpen) && 'gap-2')}>
+                <div className={cn('w-full transition-[width] duration-200 overflow-hidden min-w-fit', !isSidebarOpen && 'w-0 min-w-0')}>
+                  <div className='min-w-fit py-3 opacity-85 hover:opacity-100 flex gap-2'>
+                    <Button variant='outline' size='icon' className="active:opacity-50">
+                      <BellIcon className='min-w-fit' size={20} strokeWidth={1.75} />
+                    </Button>
+                    <Button variant='outline' size='icon' className="active:opacity-50" to='/historial'>
+                      <HistoryIcon className='min-w-fit' size={20} strokeWidth={1.75} />
+                    </Button>
+                    <a href={DOC_URL} target='_blank' rel="noreferrer">
+                      <Button variant='outline' size='icon' className="active:opacity-50">
+                        <HelpCircleIcon className='min-w-fit' size={20} strokeWidth={1.75} />
                       </Button>
-                    </li>
-                    <li>
-                      <Button variant='outline' size='icon' className="focus:outline-none focus:ring-2 focus:ring-gray-300" to='/historial'>
-                        <HistoryIcon size={20} strokeWidth={1.75} />
-                      </Button>
-                    </li>
-                    <li>
-                      <a href={DOC_URL} target='_blank' rel="noreferrer">
-                        <Button variant='outline' size='icon' className="focus:outline-none focus:ring-2 focus:ring-gray-300">
-                          <HelpCircleIcon size={20} strokeWidth={1.75} />
-                        </Button>
-                      </a>
-                    </li>
-                    <li>
-                      <Button variant='outline' className="focus:outline-none focus:ring-2 focus:ring-gray-300" onClick={onLogout}>
-                        <LogOutIcon className='mr-2' size={20} strokeWidth={1.75} />
-                        Salir
-                      </Button>
-                    </li>
-                  </ul>
+                    </a>
+                  </div>
                 </div>
+                <Button
+                  variant='outline'
+                  className="w-full"
+                  onClick={onLogout}
+                >
+                  <LogOutIcon className='min-w-fit' size={20} strokeWidth={1.75} />
+                  <span className={cn('w-auto transition-all duration-200 overflow-hidden', !isSidebarOpen ? 'w-0 opacity-0' : 'ml-1')}>Salir</span>
+                </Button>
+              </div>
 
-                <Separator orientation='horizontal' />
+              <Separator orientation='horizontal' />
 
-                <div className='flex-grow-0 text-muted-foreground py-3 px-4' style={{ fontSize: 11 }}>
-                  © {getYear(Date())} - <a className='text-xs font-semibold text-muted-foreground hover:underline underline-offset-2' href={COPYRIGHT.URL} target='_blank' rel="noreferrer">{COPYRIGHT.NAME}</a>
-                </div>
+              <div className='flex-grow-0 flex gap-1 text-muted-foreground py-2 px-4 whitespace-nowrap' style={{ fontSize: 11 }}>
+                <span>© {getYear(Date())}</span>
+                <span className={cn('w-full transition-[width] duration-200 overflow-hidden', !isSidebarOpen && 'w-0')}> - <a className='font-semibold text-muted-foreground hover:underline underline-offset-2' href={COPYRIGHT.URL} target='_blank' rel="noreferrer">{COPYRIGHT.NAME}</a></span>
               </div>
             </div>
-          </ScrollArea>
+          </div>
+        </ScrollArea>
       </aside>
     </>
   )
