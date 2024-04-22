b19b32e8989dbab6b40bc7ffd58ff0871723fbfe
Luis Arrieta <luisarrieta796@gmail.com>
2024-04-22 16:14:08 -0500

    Fix layout shift in sparkLine charts on dashboard

--- src/components/pages/DashboardPage.tsx
+++ src/components/pages/DashboardPage.tsx
@@ -1,7 +1,6 @@
 import ApexChart from '@/components/ui/chart'
 import CardTrendWidget from '@/components/widgets/CardTrendWidget'
 import { useAuth } from '@/hooks/useAuth'
-import { donutOptions } from '@/lib/apex-charts/donutOptions'
 import { type ApexOptions } from 'apexcharts'
 
 export default function DashboardPage (): JSX.Element {
@@ -21,6 +20,56 @@ export default function DashboardPage (): JSX.Element {
   }]
 
   const donutSeries = [44, 55, 41, 17, 15]
+  const donutOptions: ApexOptions = {
+    chart: {
+      fontFamily: 'Lato'
+    },
+    plotOptions: {
+      pie: {
+        donut: {
+          size: '70%',
+          labels: {
+            show: true,
+            name: {
+              show: true,
+              offsetY: 35
+            },
+            value: {
+              show: true,
+              color: 'hsl(var(--card-foreground))',
+              fontWeight: 600,
+              fontSize: '2.2rem',
+              offsetY: -10
+            },
+            total: {
+              show: true,
+              showAlways: true,
+              color: 'hsl(var(--muted-foreground))',
+              fontWeight: 200,
+              fontSize: '1.2rem'
+            }
+          }
+        }
+      }
+    },
+    stroke: {
+      colors: ['hsl(var(--card))'],
+      width: 4
+    },
+    legend: {
+      position: 'right',
+      itemMargin: {
+        horizontal: 0,
+        vertical: 4
+      },
+      fontWeight: 500,
+      fontSize: '16',
+      formatter (legendName) {
+        return `<span class="text-muted-foreground ml-1 pr-10">${legendName}<span>`
+      },
+      offsetY: 20
+    }
+  }
 
   const lineOptions: ApexOptions = {
     chart: {
@@ -102,16 +151,14 @@ export default function DashboardPage (): JSX.Element {
               series={areaSeries}
             />
           </div>
-          <div className='shrink-0 bg-card rounded-lg order-3 p-5 flex justify-center overflow-x-auto lg:overflow-hidden'>
-            <div className='min-w-fit'>
-              <ApexChart
-                width='450'
-                height='350'
-                type="donut"
-                series={donutSeries}
-                options={donutOptions}
-              />
-            </div>
+          <div className='bg-card rounded-lg order-3 p-5 flex justify-center items-center overflow-x-auto lg:overflow-hidden' style={{ minWidth: '30rem' }}>
+            <ApexChart
+              width='430'
+              height='350'
+              type="donut"
+              series={donutSeries}
+              options={donutOptions}
+            />
           </div>
         </div>
 
--- src/components/widgets/CardTrendWidget.tsx
+++ src/components/widgets/CardTrendWidget.tsx
@@ -1,8 +1,6 @@
 import { Icon, icons } from '@/components/icons/Icons'
 import ApexChart, { type ApexChartProps } from '@/components/ui/chart'
-import { areaSparklineOptions } from '@/lib/apex-charts/areaOptions'
 import { cn } from '@/lib/utils'
-import { mergeObjects } from '@/lib/utils/handleObjects'
 import { type ApexOptions } from 'apexcharts'
 
 interface Props extends ApexChartProps {
@@ -18,11 +16,23 @@ export default function CardTrend ({ title, value, ...props }: Props): JSX.Eleme
   const isPositiveTrend = trendPercentage >= 0
 
   const chartOptions: ApexOptions = {
+    chart: {
+      sparkline: { enabled: true },
+      fontFamily: 'Lato'
+    },
     stroke: {
+      curve: 'smooth',
+      width: 2,
       colors: [isPositiveTrend ? 'rgb(0, 227, 150)' : 'rgb(248, 113, 113)']
     },
     fill: {
+      type: 'gradient',
       gradient: {
+        shadeIntensity: 1,
+        inverseColors: false,
+        opacityFrom: 0.4,
+        opacityTo: 0,
+        stops: [40, 100],
         colorStops: [
           {
             offset: 0,
@@ -36,23 +46,27 @@ export default function CardTrend ({ title, value, ...props }: Props): JSX.Eleme
           }
         ]
       }
-    }
+    },
+    labels: [...Array(24).keys()].map(n => `2018-09-0${n + 1}`),
+    tooltip: { theme: 'dark' }
   }
+
   return (
-    <div className='max-h-40 p-5 bg-card rounded-lg flex items-end justify-between overflow-hidden'>
-      <div className='w-fit h-full flex flex-col'>
-        <span className='text-lg opacity-70 font-light tracking-wide'>{title}</span>
+    <div className='max-h-40 p-5 bg-card rounded-lg grid grid-cols-2 items-end overflow-hidden' style={{ gridTemplateColumns: 'auto 1fr' }}>
+      <div className='w-fit h-full flex flex-col pr-4'>
+        <span className='text-md opacity-70 font-light tracking-wide'>{title}</span>
         <strong className='grow text-3xl font-semibold'>{value.after}</strong>
-        <span className={cn('h-fit flex items-center gap-2', isPositiveTrend ? 'text-emerald-500' : 'text-red-400')} title={`${trendPercentage.toFixed(2)}%`}>
+        <span className={cn('h-fit flex items-center gap-2 text-sm', isPositiveTrend ? 'text-emerald-500' : 'text-red-400')} title={`${trendPercentage.toFixed(2)}%`}>
           {isPositiveTrend && '+'}{trendPercentage.toFixed(2)}%
           <Icon element={icons.ArrowTrend.Up} />
         </span>
       </div>
       <ApexChart
-        width='90%'
-        height='90%'
+        className=''
+        width='100%'
+        height='70%'
         type="area"
-        options={mergeObjects(chartOptions, areaSparklineOptions)}
+        options={chartOptions}
         {...props}
       />
     </div>
