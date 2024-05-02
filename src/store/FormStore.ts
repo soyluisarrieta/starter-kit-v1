49fb48ee4e6ec8a982e2f56450ca685575a8ad0a
Luis Arrieta <luisarrieta796@gmail.com>
2024-05-01 21:41:58 -0500

    Add some ajusts to form store

--- src/store/FormStore.ts
+++ src/store/FormStore.ts
@@ -1,25 +1,60 @@
 import { create } from 'zustand'
 
+export interface FormHeader { title: string | null, description?: string | null }
+
 interface FormStore {
+  formHeader: FormHeader
+  isResetedFormStore: boolean
   isFormModified: boolean
   onSubmitExists: boolean
   timestamps: { updatedAt?: string | null, createdAt?: string | null }
+
+  setFormHeader: (formHeader: FormHeader) => void
   onSubmit: ((data: any) => void) | undefined
-  onReset: () => void
   setIsFormModified: (isFormModified: boolean) => void
   setOnSubmit: (onSubmit: (data: any) => void) => void
-  setOnReset: (onSubmit: () => void) => void
   setTimestamps: (timestamps: { updatedAt?: string | null, createdAt?: string | null }) => void
+  setOnResetForm: (onSubmit: () => void) => void
+  onResetForm: () => void
+  resetStore: () => void
 }
 
 export const useFormStore = create<FormStore>((set) => ({
+  formHeader: { title: null, description: null },
+  isResetedFormStore: true,
   isFormModified: false,
   onSubmitExists: false,
-  timestamps: { updatedAt: null, createdAt: null },
   onSubmit: undefined,
-  onReset: () => {},
-  setIsFormModified: (isFormModified) => { set({ isFormModified }) },
-  setOnSubmit: (onSubmit) => { set({ onSubmit }) },
-  setOnReset: (onReset) => { set({ onReset }) },
-  setTimestamps: (timestamps) => { set({ timestamps }) }
+  timestamps: { updatedAt: null, createdAt: null },
+  onResetForm: () => {},
+
+  setIsFormModified: (isFormModified) => {
+    set({ isFormModified, isResetedFormStore: false })
+  },
+  setOnSubmit: (onSubmit) => {
+    set({ onSubmit, isResetedFormStore: false, onSubmitExists: true })
+  },
+  setTimestamps: (timestamps) => {
+    set({ timestamps, isResetedFormStore: false })
+  },
+  setOnResetForm: (onResetForm) => {
+    set({ onResetForm, isResetedFormStore: false })
+  },
+  resetStore: () => {
+    set({
+      formHeader: { title: null, description: null },
+      isResetedFormStore: true,
+      isFormModified: false,
+      onSubmitExists: false,
+      onSubmit: undefined,
+      timestamps: { updatedAt: null, createdAt: null },
+      onResetForm: () => {}
+    })
+  },
+  setFormHeader: (formHeader) => {
+    set(({ resetStore }) => {
+      resetStore()
+      return { formHeader }
+    })
+  }
 }))
