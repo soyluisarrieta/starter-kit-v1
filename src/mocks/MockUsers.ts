8121acad96014ae0d6a79a1e972b8415ed0f4acb
Luis Arrieta <luisarrieta796@gmail.com>
2025-01-06 08:36:06 -0500

    Install @tanstack/react-table and create users mock

---
+++ src/mocks/MockUsers.ts
@@ -0,0 +1,42 @@
+export const MockUsers: ProfileAuth[] = [
+  {
+    id: '1',
+    name: 'John',
+    last_name: 'Doe',
+    full_name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    created_at: '2025-07-01T00:00:00.000Z',
+    updated_at: '2025-07-12T00:00:00.000Z',
+    active: true
+  },
+  {
+    id: '2',
+    name: 'Jane',
+    last_name: 'Doe',
+    full_name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    created_at: '2025-07-10T00:00:00.000Z',
+    updated_at: '2025-07-29T00:00:00.000Z',
+    active: false
+  },
+  {
+    id: '3',
+    name: 'John',
+    last_name: 'Smith',
+    full_name: 'John Doe',
+    email: 'johnsmith@gmail.com',
+    created_at: '2025-06-03T00:00:00.000Z',
+    updated_at: '2025-07-06T00:00:00.000Z',
+    active: false
+  },
+  {
+    id: '4',
+    name: 'Jane',
+    last_name: 'Smith',
+    full_name: 'John Doe',
+    email: 'janesmith@gmail.com',
+    created_at: '2025-03-10T00:00:00.000Z',
+    updated_at: '2025-10-27T00:00:00.000Z',
+    active: true
+  }
+]
