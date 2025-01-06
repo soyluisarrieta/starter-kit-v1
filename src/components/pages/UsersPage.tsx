71db23aca64f67557cd06b7a3c46382a4f8bc992
Luis Arrieta <luisarrieta796@gmail.com>
2025-01-06 09:22:48 -0500

    Create List as CRUD page component

--- src/components/pages/UsersPage.tsx
+++ src/components/pages/UsersPage.tsx
@@ -1,11 +1,24 @@
-import { Button } from '@/components/ui/button'
-import { Link } from 'wouter'
+import PageLayout from '@/components/layouts/PageLayout'
+import List from '@/components/pages/CRUD/List'
+import { MockUsers } from '@/mocks/MockUsers'
 
 export default function UsersPage (): JSX.Element {
   return (
-    <div>UsersPage
-      <br />
-      <Link to='/'><Button>Dashboard</Button></Link>
-    </div>
+    <PageLayout
+      title="Usuarios"
+      description='Lista de usuarios del sistema'
+    >
+      <main>
+        <List
+          className='container'
+          data={MockUsers}
+          columns={[
+            { header: 'ID', accessorKey: 'id' },
+            { header: 'Nombre completo', accessorKey: 'full_name' },
+            { header: 'Correo electrónico', accessorKey: 'email' }
+          ]}
+        />
+      </main>
+    </PageLayout>
   )
 }
