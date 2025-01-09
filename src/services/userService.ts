9985f43ee5f3ecc60033a2fb52859f021f7684ce
Luis Arrieta <luisarrieta796@gmail.com>
2025-01-08 20:27:59 -0500

    Create user crud services

---
+++ src/services/userService.ts
@@ -0,0 +1,25 @@
+import axios from '@/lib/axios'
+
+export const getUsersService = async (): Promise<ProfileAuth[]> => {
+  const { data } = await axios.get('/api/users')
+  return data
+}
+
+export const getUserService = async (id: string): Promise<ProfileAuth> => {
+  const { data } = await axios.get(`/api/users/${id}`)
+  return data
+}
+
+export const createUserService = async (user: ProfileAuth): Promise<ProfileAuth> => {
+  const { data } = await axios.post('/api/users', user)
+  return data
+}
+
+export const updateUserService = async (user: ProfileAuth): Promise<ProfileAuth> => {
+  const { data } = await axios.put(`/api/users/${user.id}`, { ...user })
+  return data
+}
+
+export const deleteUserService = async (id: string): Promise<void> => {
+  await axios.delete(`/api/users/${id}`)
+}
