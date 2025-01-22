c5146fe129262d6e5a46ec5d7aa39a6169823ac7
Luis Arrieta <luisarrieta796@gmail.com>
2025-01-22 15:47:27 -0500

    Return user data with role names

--- API_LaravelBreeze/routes/api.php
+++ API_LaravelBreeze/routes/api.php
@@ -3,11 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Resources\UserResource;
 
 Route::middleware(['auth:sanctum'])->group(function () {
   // Get authenticated user
   Route::get('/user', function (Request $request) {
-    return $request->user();
+    return UserResource::make(Auth::user());
   });
 
   // Resources
