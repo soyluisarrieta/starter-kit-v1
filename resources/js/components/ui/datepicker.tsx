7d0ad93b1ad0d870d52b1e55b6c9235c3b3858b3
soyluisarrieta <luisarrieta796@gmail.com>
2025-03-12 13:47:27 -0500

    feat: add datepicker component

--- resources/js/components/ui/datepicker.tsx
+++ resources/js/components/ui/datepicker.tsx
@@ -1,6 +1,5 @@
-import { format } from 'date-fns'
+import { format, parseISO } from 'date-fns'
 import { es } from 'date-fns/locale'
-import * as React from 'react'
 
 import { Button } from '@/components/ui/button'
 import { Calendar, CalendarProps } from '@/components/ui/calendar'
@@ -14,19 +13,12 @@ import { CalendarIcon } from 'lucide-react'
 import { PropsSingle } from 'react-day-picker'
 
 type DatePickerProps = Omit<CalendarProps, 'selected'> & Omit<PropsSingle, 'onSelect' | 'selected'> & {
-  value?: Date
-  onChange: (date: Date | undefined) => void
+  value?: string
+  onChange: (date: string | undefined) => void
   placeholder?: string
 }
 
 export default function DatePicker ({ mode = 'single', value, onChange, placeholder, ...props }: DatePickerProps) {
-  const [date, setDate] = React.useState(value)
-
-  const handleSelect = (selected: Date | undefined) => {
-    setDate(selected)
-    onChange(selected)
-  }
-
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -34,20 +26,21 @@ export default function DatePicker ({ mode = 'single', value, onChange, placehol
           variant={'outline'}
           className={cn(
             'w-[240px] justify-start text-left font-normal cursor-pointer',
-            !date && 'text-muted-foreground'
+            !value && 'text-muted-foreground'
           )}
         >
           <CalendarIcon className="mr-1 h-4 w-4" />
-          {date
-            ? <span className='capitalize'>{format(date, 'MMMM dd, yyyy', { locale: es })}</span>
+          {value
+            ? <span className='capitalize'>{format(parseISO(value), 'MMMM dd, yyyy', { locale: es })}</span>
             : <span>{placeholder ?? 'Selecciona una fecha'}</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0 [&_button]:cursor-pointer" align="start">
         <Calendar
           mode={mode}
-          selected={date}
-          onSelect={handleSelect}
+          selected={value ? parseISO(value) : undefined}
+          onSelect={(date) => onChange(date ? format(date, 'yyyy-MM-dd') : undefined)}
+          locale={es}
           {...props}
         />
       </PopoverContent>
