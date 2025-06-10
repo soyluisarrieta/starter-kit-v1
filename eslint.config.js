9535664c2a44ead66b4a85eeb4607beb802d148e
soyluisarrieta <luisarrieta796@gmail.com>
2025-06-10 18:54:03 -0500

    build: add @soyluisarrieta/eslint package and update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,16 @@
 import js from '@eslint/js';
-import prettier from 'eslint-config-prettier';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
 import typescript from 'typescript-eslint';
+import soyluisarrietaESlint from '@soyluisarrieta/eslint'
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
     js.configs.recommended,
     ...typescript.configs.recommended,
+    soyluisarrietaESlint,
     {
         ...react.configs.flat.recommended,
         ...react.configs.flat['jsx-runtime'], // Required for React 17+
@@ -38,7 +40,14 @@ export default [
         },
     },
     {
-        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js'],
-    },
-    prettier, // Turn off all rules that might conflict with Prettier
+        ignores: [
+          'vendor', 
+          'node_modules', 
+          'public', 
+          'bootstrap/ssr', 
+          'tailwind.config.js', 
+          'dist',
+          'resources/js/components/ui'
+        ],
+    }
 ];
