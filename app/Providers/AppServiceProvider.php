7794afe2e941ad3eae5ba31da8879a8f0ecf45fa
soyluisarrieta <luisarrieta796@gmail.com>
2025-06-11 14:23:31 -0500

    feat(routing): add spanish resource route macro to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Route::macro('resource_es', function ($name, $controller) {
+            Route::get($name, [$controller, 'index'])->name("$name.index");
+            Route::get("$name/crear", [$controller, 'create'])->name("$name.crear");
+            Route::post($name, [$controller, 'store'])->name("$name.guardar");
+            Route::get("$name/{id}", [$controller, 'show'])->name("$name.mostrar");
+            Route::get("$name/{id}/editar", [$controller, 'edit'])->name("$name.editar");
+            Route::put("$name/{id}", [$controller, 'update'])->name("$name.actualizar");
+            Route::delete("$name/{id}", [$controller, 'destroy'])->name("$name.eliminar");
+        });
     }
 }
