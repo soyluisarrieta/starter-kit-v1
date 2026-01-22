e9dcb3c4161d6ed3ac7702a15ed591b1e6b40183
Luis Arrieta <luisarrieta796@gmail.com>
2026-01-22 18:07:24 -0500

    fix: update home redirection

--- tests/Feature/Auth/VerificationNotificationTest.php
+++ tests/Feature/Auth/VerificationNotificationTest.php
@@ -22,7 +22,7 @@ class VerificationNotificationTest extends TestCase
 
         $this->actingAs($user)
             ->post(route('verification.send'))
-            ->assertRedirect(route('home'));
+            ->assertRedirect(route('dashboard'));
 
         Notification::assertSentTo($user, VerifyEmail::class);
     }
--- tests/Feature/Settings/ProfileUpdateTest.php
+++ tests/Feature/Settings/ProfileUpdateTest.php
@@ -73,7 +73,7 @@ class ProfileUpdateTest extends TestCase
 
         $response
             ->assertSessionHasNoErrors()
-            ->assertRedirect(route('home'));
+            ->assertRedirect(route('dashboard'));
 
         $this->assertGuest();
         $this->assertNull($user->fresh());
