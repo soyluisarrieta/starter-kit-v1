31ede947946cbb919a6a6bb45bedca50c3407a20
Luis Arrieta <luisarrieta796@gmail.com>
2026-01-23 12:38:11 -0500

    feat: protect user routes with permission middleware

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,19 @@
 <?php
 
+use App\Enums\Permissions;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', fn () => Inertia::render('dashboard'))->name('dashboard');
 
-    Route::get('/usuarios', fn () => Inertia::render('users/index'))->name('users');
-    Route::get('/usuarios/crear', fn () => Inertia::render('users/create'))->name('users.create');
+    Route::get('/usuarios', fn () => Inertia::render('users/index'))
+        ->middleware('can:' . Permissions::LIST_USER->value)
+        ->name('users');
+
+    Route::get('/usuarios/crear', fn () => Inertia::render('users/create'))
+        ->middleware('can:' . Permissions::CREATE_USER->value)
+        ->name('users.create');
 });
 
 require __DIR__.'/auth.php';
