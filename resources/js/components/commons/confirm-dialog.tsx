d892d0fa235ce6682f3ac2445380d9be5eb65f49
Luis Arrieta <luisarrieta796@gmail.com>
2026-02-02 14:12:23 -0500

    feat: add confirm dialog component

---
+++ resources/js/components/commons/confirm-dialog.tsx
@@ -0,0 +1,122 @@
+import { useForm } from '@inertiajs/react';
+import { LoaderIcon } from 'lucide-react';
+import type { ReactNode } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+    Dialog,
+    DialogTrigger,
+    DialogContent,
+    DialogTitle,
+    DialogDescription,
+    DialogFooter,
+    DialogClose,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import InputError from '@/components/ui/input-error';
+
+interface ConfirmDialogProps {
+    open?: boolean;
+    onOpenChange?: (open: boolean) => void;
+    title: string;
+    description?: string;
+    passwordRequired?: boolean;
+    method: 'post' | 'put' | 'delete';
+    url: string | null;
+    children?: ReactNode;
+}
+
+export function ConfirmDialog({
+    open,
+    onOpenChange,
+    title,
+    description,
+    passwordRequired,
+    method,
+    url,
+    children,
+}: ConfirmDialogProps) {
+    const {
+        data,
+        setData,
+        processing,
+        errors,
+        reset,
+        clearErrors,
+        [method]: send,
+    } = useForm({
+        password: '',
+    });
+
+    // Submit with password
+    const onSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!url) return;
+        clearErrors('password');
+
+        send(url, {
+            preserveScroll: true,
+            onSuccess: () => {
+                onOpenChange?.(false);
+                reset('password');
+            },
+        });
+    };
+
+    return (
+        <Dialog open={open} onOpenChange={onOpenChange}>
+            {children && <DialogTrigger asChild>{children}</DialogTrigger>}
+
+            <DialogContent>
+                <DialogTitle>{title}</DialogTitle>
+                {description && (
+                    <DialogDescription>
+                        {description}{' '}
+                        {passwordRequired &&
+                            'Por favor ingresa tu contraseña para confirmar.'}
+                    </DialogDescription>
+                )}
+                <form onSubmit={onSubmit}>
+                    {passwordRequired && (
+                        <div className="mb-4 grid gap-2">
+                            <Input
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoFocus
+                                tabIndex={1}
+                                value={data.password}
+                                onChange={(e) => {
+                                    setData('password', e.target.value);
+                                    clearErrors('password');
+                                }}
+                                disabled={processing}
+                                placeholder="Ingrese su contraseña"
+                            />
+                            <InputError message={errors.password} />
+                        </div>
+                    )}
+                    <DialogFooter className="gap-2">
+                        <DialogClose asChild>
+                            <Button variant="secondary" disabled={processing}>
+                                Cancelar
+                            </Button>
+                        </DialogClose>
+
+                        <Button
+                            type="submit"
+                            variant="destructive"
+                            onClick={onSubmit}
+                            disabled={processing}
+                        >
+                            {processing && (
+                                <LoaderIcon className="animate-spin" />
+                            )}
+                            {processing ? 'Procesando' : 'Confirmar'}
+                        </Button>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+    );
+}
